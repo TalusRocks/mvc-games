3a5ca5793db83c809a9de35bc8e7038371a6227c
RJ <jensenrs@gmail.com>
2017-10-20 15:51:20 -0700

    routes work, no verification yet

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -1,23 +1,38 @@
 const model = require('../models/game')
 
 function getAll(req, res, next) {
-  res.status(200).json({message:"whatever"})
+  const data = model.getAll()
+
+  res.status(200).json({data})
 }
 
 function getOne(req, res, next) {
-  next()
+  const id = req.params.id
+  console.log(req.params);
+  const data = model.getOne(id)
+
+  res.status(200).json({ data })
 }
 
 function create(req, res, next) {
-  next()
+  const data = model.create(req.body)
+
+  res.status(201).json({ data })
 }
 
 function update(req, res, next) {
-  next()
+  const id = req.params.id
+  const body = req.body
+  const data = model.update(id, body)
+
+  res.status(200).json({ data })
 }
 
 function destroy(req, res, next) {
-  next()
+  const id = req.params.id
+  const data = model.destroy(id)
+
+  res.status(200).json({ data })
 }
 
 module.exports = { getAll, getOne, create, update, destroy }
--- src/models/game.js
+++ src/models/game.js
@@ -1,37 +1,61 @@
 const uuid = require('uuid/v4')
 const games = [
-  {name: "Scythe",
-  weight: 3.34,
-  rating: 8.3,
-  own: true},
-  {name: "Terraforming Mars",
-  weight: 3.24,
-  rating: 8.4,
-  own: true},
-  {name: "Azul",
-  weight: 2.00,
-  rating: 7.8,
-  own: false}
+  {
+    name: "Scythe",
+    weight: 3.34,
+    own: true,
+    id: uuid()
+  },
+  {
+    name: "Terraforming Mars",
+    weight: 3.24,
+    own: true,
+    id: uuid()
+  },
+  {
+    name: "Azul",
+    weight: 2.00,
+    own: false,
+    id: 'd7a0ddfe-e5b8-44f5-a3e6-0fea59c53394'
+  }
 ]
 
 function getAll() {
-
+  return games
 }
 
-function getOne() {
-
+function getOne(id) {
+  return games.find(el => el.id === id)
 }
 
-function create() {
+function create(body) {
+  const name = body.name
+  const weight = body.weight
+  const own = body.own
+  const id = uuid()
 
+  const game = { name, weight, own, id }
+  games.push(game)
+  return game
 }
 
-function update() {
+function update(id, body) {
+  const game = games.find(game => game.id === id)
 
+  game.name = body.name
+  game.weight = body.weight
+  game.own = body.own
+
+  return game
 }
 
-function destroy() {
+function destroy(id) {
+  const game = games.find(game => game.id === id)
 
+  let index = games.indexOf(game)
+  let deletedGame = games.splice(index, 1)
+
+  return deletedGame
 }
 
 module.exports = { getAll, getOne, create, update, destroy }
